35d17d390ac8452f04db170f0e0fab4c8e3df38e
Mikhailova_ES <Mikhailova_ES@utkonos.ru>
2020-02-13 20:48:48 +0300

    script/start.sh upd

--- script/start.sh
+++ script/start.sh
@@ -1,1 +1,4 @@
+#docker-compose -f docker/docker-compose.yml up -d
+docker-compose -f docker/docker-compose.yml down
+docker-compose -f docker/docker-compose.yml build --no-cache
 docker-compose -f docker/docker-compose.yml up -d
