af9fd86ac4a4bcf11558e6d950f1fc40c94131a2
Mikhailova_ES <Mikhailova_ES@utkonos.ru>
2020-02-15 13:52:43 +0300

    uod for server and add task entity

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/elizavetamikhailova/TasksProject/api"
 	"github.com/elizavetamikhailova/TasksProject/cmd/di"
 	"github.com/elizavetamikhailova/TasksProject/configs"
@@ -10,9 +11,10 @@ import (
 
 func main() {
 	cfg := configs.Config{}
-	if err := configor.Load(&cfg, "configs/config.prod1.yaml", "configs/config.yaml"); err != nil {
+	if err := configor.Load(&cfg, "configs/config.prod.yaml", "configs/config.yaml"); err != nil {
 		panic(err)
 	}
+	fmt.Println(cfg)
 	container := di.GetDI(cfg)
 	route := new(api.Router).Get(container)
 	println("Hello!")
